0c569af7a85a86f610a781941ef50549f5f5bdb3
jayasri r <jayasriraja0@gmail.com>
2025-08-10 15:08:47 +0530

    modified leapornot.java and greatestofthreenumber.java

--- greatestofthreenumber.java
+++ greatestofthreenumber.java
@@ -2,6 +2,7 @@ public class greatestofthreenumber
 {
     public static void main(String[] args)
     {
+        int a=10;
         
     }
 }
--- leapornot.java
+++ leapornot.java
@@ -2,4 +2,5 @@ package example;
 
 public class leapornot {
     int a=10;
+    int c=20;
 }
