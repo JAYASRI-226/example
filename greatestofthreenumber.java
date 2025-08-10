9b12b4a9dfbd2602dd136d3ca0fb893a7c5ebe64
jayasri r <jayasriraja0@gmail.com>
2025-08-10 17:19:11 +0530

    modified

--- greatestofthreenumber.java
+++ greatestofthreenumber.java
@@ -1,8 +1,9 @@
+package example;
 public class greatestofthreenumber
 {
     public static void main(String[] args)
     {
         int a=10;
-        
+
     }
 }
--- leapornot.java
+++ leapornot.java
@@ -1,6 +1,17 @@
 package example;
-
+import java.util.Scanner;
 public class leapornot {
-    int a=10;
-    int c=20;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    int year=sc.nextInt();
+    if(year%400==0 || year%4==0 && year%100!=0)
+    {
+        System.out.println("it is leap year");
+    }
+    else
+    {
+        System.out.println("it is not a leap year");
+    }
+    }
+    
 }
