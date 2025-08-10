2c142839f8ff58da2797fc34752781699e704afa
jayasri r <jayasriraja0@gmail.com>
2025-08-10 17:49:02 +0530

    modified

--- Primenumber.java
+++ Primenumber.java
@@ -1,5 +1,33 @@
 package example;
 
 public class Primenumber {
+    public static void main(String[] args)
+    {
+        int n=7;
+    boolean prime=true;
+    if(n<=1)
+    {
+        prime=false;
+    }
+    else
+    {
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                prime=false;
+                break;
+            }
+        }
+    }
+    if(prime)
+    {
+        System.out.println("It is prime number");
+    }
+    else
+    {
+        System.out.println("It is not a prime number");
+    }
+    }
     
 }
